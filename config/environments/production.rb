85f19761b1fb356adbe606557b7ac19f4efe99da
Kopylov German <roverrr@gmail.com>
2015-04-08 19:45:04 +0300

    Improve static content handle

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,9 +18,10 @@ Rails.application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
+  config.action_dispatch.rack_cache = ENV['RAILS_RACK_CACHE'].present?
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
